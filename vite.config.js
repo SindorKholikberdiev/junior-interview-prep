4ae7366299384a43650f7c928508dbb7d088a468
SindorKholikberdiev <sindorkholikberdiev@gmail.com>
2025-10-27 03:28:52 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  // 'command' bizga 'serve' (npm run dev) yoki 'build' (npm run build) ekanligini aytadi
+
+  // Agar 'build' buyrug'i bo'lsa (ya'ni deploy uchun),
+  // 'base' manzilini repository nomiga tenglaymiz.
+  // Aks holda ('serve' paytida), 'base' ildiz ('/') bo'ladi.
+  const base = command === "build" ? "/junior-interview-prep/" : "/";
+
+  return {
+    base: base,
+    plugins: [react()],
+  };
 });
